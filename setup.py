e672aa18d36244d2ff3047b345fab1995076fa7d
Peter Skipper <peter.skipper@gmail.com>
2018-02-07 14:29:36 -0600

    new requirements

--- setup.py
+++ setup.py
@@ -10,8 +10,12 @@ setup(
     url="https://github.com/peterskipper/pizzabutton",
     author="Peter Skipper",
     author_email="peter.skipper@gmail.com",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
+    install_requires=[
+        "pizzapi==0.0.3",
+        "jupyter==1"
+    ]
     description="A button on Jupyter's toolbar for pizza",
     long_description=long_desc,
 )
